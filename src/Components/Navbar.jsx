61fab94cc1a2d037eaca6725a1b4efc445fb81b8
az4mieee <zamiwahabsalam@gmail.com>
2025-09-18 19:59:13 +0700

    perbaikan typo huruf pada file Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; 
 
-export default function Navba() {
+export default function Navbar() {
   const navigate = useNavigate(); 
 
   return (
